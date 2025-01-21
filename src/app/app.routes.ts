e5dc67bd9204b02ffcca318d08c19349fd3f6639
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2025-01-21 14:40:06 +0100

    Init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TablaPersonasComponent } from './components/tabla-personas/tabla-personas.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'tabla', component: TablaPersonasComponent},
+    {path: 'formulario', component: FormularioComponent}
+];
