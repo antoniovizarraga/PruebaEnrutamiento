215f2701dff956c6d6775e8ada2ea99985c465f9
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2025-01-28 14:32:56 +0100

    Integración de Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterOutlet, RouterLink, Router } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,4 +17,7 @@ export class AppComponent {
   
   title = 'pruebaEnrutamiento';
 }
+
+export class ButtonOverviewExample {}
+
 
